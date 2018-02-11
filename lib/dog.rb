0387ceb8370c452e4774bbebadf2cb41e2e43418
Chely Ho <adept_inept@yahoo.com>
2018-02-11 18:31:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear           # or @@all = [] or @@all.delete_all
+  end
+
+  def self.all
+    @@all.each {|dog| puts dog.name}
+  end
+end
